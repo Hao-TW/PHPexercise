d07149d51f365dd449e345947e31da92b1ce7201
Hao <mfisgood@gmail.com>
2019-12-05 20:57:39 +0800

    new files

---
+++ ch6/test1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>first test</title>
+</head>
+<body>
+    <?php
+    printf("Hello world")
+    ?>
+</body>
+</html>
