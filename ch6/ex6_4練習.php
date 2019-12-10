0cbbba8eace65cac5a2b82de822633ebaf1424ef
Hao-TW <55446816+Hao-TW@users.noreply.github.com>
2019-12-10 21:32:39 +0800

    Update ex6_4練習.php

--- ch6/ex6_4練習.php
+++ ch6/ex6_4練習.php
@@ -3,12 +3,12 @@
     function hello($x){
         $sum = $x*2;
         echo "函數執行結束<br />";
-        return $sum
+        return $sum;
     }
 ?>
 <p> ----分隔線----<P>
 <?php
-    $sum=hello($x);
+    $sum = hello($x);
     echo "x=$x<br />";
     echo "sum=$sum<br />";
 
