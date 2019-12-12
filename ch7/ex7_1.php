ad071fa423b8c760088ae95e4276440cf903c5df
root <root@localhost.localdomain>
2019-12-12 22:24:41 +0800

    test

--- ch7/ex7_1.php
+++ ch7/ex7_1.php
@@ -1,34 +1,11 @@
 <?php
-  // 顯示名字
-  function name(){
-      echo "Peter";
-  }
+    function sum(...$numbers){
+        $total = 0;
+        foreach ($numbers as $i){
+            $total += $i;
+        }
+        return $total;
+    }
 
-  // 計算成績等級
-  function score($i) {
-      $j = "F";
-      $level = intval($i / 10);
-      switch ($level){
-          case 10:
-          case 9:
-            $j = "A";
-            break;
-          case 8:
-            $j = "B";
-            break;
-          case 7:
-            $j = "C";
-            break;
-          case 6:
-            $j = "D";
-            break;
-          default:
-            $j = "E";
-      }
-      return $j;
-  }
-
-  echo name();
-  $backscore = score(85);
-  echo "　成績等級：$backscore";
+    echo "總共是：".sum(1,3,5,7,9);
 ?>
---
+++ ch7/ex7_222.php
@@ -0,0 +1,19 @@
+<?php
+function sum(...$n){
+    $total=0;
+    foreach($n as $i){
+        $total+=$i;
+    }
+    return $total;
+}
+print"TOTAL: ".sum(1,3,7,11,9);
+?>
+<?php
+  // 從 1 加到 50 的整數和
+  // 以下方式只是為了說明 for 迴圈的使用方式，不是好的程式碼寫法！！
+  $sum = 0;
+  for ($i = 1; $i <= 50; i++){
+      $sum += $i;
+  }  
+  echo "總合是: $sum";
+?>
