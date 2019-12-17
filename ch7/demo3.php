f32b276b0db5270c3c0f602ea02e63826b9ba69c
Hao <mfisgood@gmail.com>
2019-12-17 20:47:09 +0800

    modify

--- ch7/demo3.php
+++ ch7/demo3.php
@@ -7,12 +7,5 @@
   echo $mydog->doWork()."<br />";
   //子類別未改寫父類別的方法
   echo $mydog->dogRun()."<br />";
-  //print 測試
-  print $mydog->name."<br />";
-  print $mydog->color."<br />";
-  print $mydog->style."<br />";
-  print $mydog->doWork()."<br />";
-  //子類別未改寫父類別的方法
-  printf $mydog->dogRun()."<br />";
 
 ?>
