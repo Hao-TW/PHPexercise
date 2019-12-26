44dd645519689d1f7c1d2aaf371130cb0d707f1f
Hao <mfisgood@gmail.com>
2019-12-26 20:44:21 +0800

    0

--- ch8/index.php
+++ ch8/index.php
@@ -1,11 +1,10 @@
 <?php
 //這是 index.php
-include_once __DIR__ . "/autoload.php";
+
 include_once __DIR__ . "/firstAutoload.php";
 include_once __DIR__ . "/secondAutoload.php";
 
-$member = new Member();
-$member->getMemberList();
+
 
 $first = new First();
 $first->doSomething();
