6d6b1010f625cb32c8ef5c37905350590206bff9
Hao <mfisgood@gmail.com>
2019-12-26 20:06:55 +0800

    modfity

--- ch8/autoload.php
+++ ch8/autoload.php
@@ -1,6 +1,6 @@
 <?php
 function __autoload($className){
-    $filename = __DIR__ ."/classes/".".php";
+    $filename = __DIR__ . "/classes/" . $className ".php";
     if (is_readable($filename)){
         require $filename;
     }
