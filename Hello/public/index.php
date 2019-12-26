b0e16da5d339d95c25071da321a8eef6aa807fb4
Hao <mfisgood@gmail.com>
2019-12-26 22:02:56 +0800

    n

--- Hello/public/index.php
+++ Hello/public/index.php
@@ -1,8 +1,19 @@
 <?php
 require_once '../vendor/autoload.php';
-
-use \Cars\Car;
+    
+use Cars\Car;
+use Customers\Customer;
+use Customers\Block\Blocklist;        
+        
 $mycar = new Car();
 $mycar->setName("Ford...");
-echo $mycar->getName();
+echo "My Car: ".$mycar->getName();
+echo "<br />";
+$myprofile = new Customer();
+$myprofile->setName("Peter");
+echo "My Name: ".$myprofile->getName();
+echo "<br />";
+$mystate = new Blocklist();
+$mystate->setState("Forzien...");
+echo $mystate->getState();
 ?>
