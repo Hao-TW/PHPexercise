29d532249e227b75ff246867369b2fa3133a4fa1
Hao <mfisgood@gmail.com>
2020-01-07 20:38:08 +0800

    new

---
+++ ch8/confuse.php
@@ -0,0 +1,24 @@
+<?php
+trait Game{
+    function play(){
+        echo 'Playing a game';
+    }
+}
+trait Music{
+    function play(){
+        echo 'Playing a music';
+    }
+}
+class Player{
+    use Game,Music{
+        Game::play as gamePlay;
+        Music::play instanceof Game;
+    }
+}
+$player = new Player();
+$player->play();
+$player->gamePlay();
+
+
+
+?>
